88c74c6da93853e6038fa04eebcc4756e4490aa7
Victor <nascimentovictor22@gmail.com>
2024-11-03 12:02:16 -0300

    add

---
+++ api/router.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/victornascimento22/api-1.0/controllers"
+)
+
+func Router1() {
+	r := gin.Default()
+
+	public := r.Group("/api")
+
+	public.POST("/register", controllers.RegisterHandler)
+
+	r.Run(":8080")
+
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/victornascimento22/api-1.0/controllers"
-)
-
 func main() {
 
-	r := gin.Default()
-
-	public := r.Group("/api")
-
-	public.POST("/register", controllers.RegisterHandler)
-
-	r.Run(":8080")
+	Router()
 
 }
